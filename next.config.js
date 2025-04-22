080edc1b5adda7541fb775643f124c5f5a8135b1
Ruifan Yu <y.refine@gmail.com>
2025-04-22 11:16:28 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['react-icons'], // Add this line
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 }
 
 module.exports = nextConfig
